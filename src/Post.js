04488463036bb59b56022e8d93d8f31b28f3033e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-02-26 12:04:47 +0100

    feat: add caption and comment functionality

--- src/Post.js
+++ src/Post.js
@@ -5,6 +5,7 @@ const BASE_URL = "http://localhost:8000/";
 
 function Post({ post }) {
   const [imageUrl, setImageUrl] = useState("");
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     if (post.image_url_type == "absolute") {
@@ -14,9 +15,23 @@ function Post({ post }) {
     }
   }, []);
 
+  useEffect(() => {
+    setComments(post.comments);
+  }, []);
+
   return (
     <div className="post">
       <img className="post_image" src={imageUrl} />
+
+      <h4 className="post_text">{post.caption}</h4>
+
+      <div className="post_comment">
+        {comments.map((comment) => (
+          <p>
+            <strong>{comment.username}:</strong> {comment.text}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
