ca2810678cda51b07b07b0a4c8f40c8d50f7df33
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-02-28 16:30:19 +0100

    feat: upload comments

--- src/App.js
+++ src/App.js
@@ -259,6 +259,7 @@ function App() {
             post={post}
             authToken={authToken}
             authTokenType={authTokenType}
+            username={username}
           />
         ))}
       </div>
--- src/Post.js
+++ src/Post.js
@@ -4,7 +4,7 @@ import { Avatar, Button } from "@material-ui/core";
 
 const BASE_URL = "http://localhost:8000/";
 
-function Post({ post, authToken, authTokenType }) {
+function Post({ post, authToken, authTokenType, username }) {
   const [imageUrl, setImageUrl] = useState("");
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState("");
@@ -43,7 +43,37 @@ function Post({ post, authToken, authTokenType }) {
       });
   };
 
-  const postComment = (event) => {};
+  const postComment = (event) => {
+    event?.preventDefault();
+
+    const json_string = JSON.stringify({
+      username: username,
+      text: newComment,
+      post_id: post.id,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: new Headers({
+        Authorization: authTokenType + " " + authToken,
+        "Content-Type": "application/json",
+      }),
+      body: json_string,
+    };
+
+    fetch(BASE_URL + "comment", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setNewComment("");
+      });
+  };
 
   return (
     <div className="post">
